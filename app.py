6ee59ad4bfc1e021c9707a9d96f2ee4f946cafbc
Nwobodo Ifeanyichukwu Victor <Eflex@users.noreply.huggingface.co>
2023-04-21 00:25:55 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import gradio as gr
+from fastai.vision.all import *
+labels = learn.dls.vocab
+
+
+def predict(img):
+    img = PILImage.create(img)
+    pred,pred_idx,probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+
+title = "Forest Fire Classifier"
+description = "A forest fire classifier trained on the dataset gotten from the internet with duckduckgo. A model was trained on the data using Fastai. Created as a demo for Gradio and HuggingFace Spaces."
+gr.Interface(fn=predict, #model function
+             inputs=gr.inputs.Image(shape=(512, 512)),#specify the input. In our case an image with
+             outputs=gr.outputs.Label(num_top_classes=2),
+             title=title,
+             description=description,
+             interpretation='default',
+             enable_queue=True).launch()
